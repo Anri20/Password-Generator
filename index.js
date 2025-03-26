c70f2d8ea91196a58b28d3cb8616682df36ccdaf
Anri20 <88647934+Anri20@users.noreply.github.com>
2025-03-26 08:31:07 +0700

    Add Character Select
    Can customize characters to be included or excluded from the password

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
-    "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
-    "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
-    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
-    "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-    "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_",
-    "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">",
-    ".", "?", "/"];
+const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", 
+    "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", 
+    "w", "x", "y", "z"];
+
+const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", 
+    "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", 
+    "W", "X", "Y", "Z"]
+
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+const symbols = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(",
+    ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", 
+    "<", ">", ".", "?", "/"]
 
 const main = document.querySelector("main")
 
 const toggle = document.getElementById("toggleSwitch")
 
 const passLength = document.getElementById("password-length")
+const includeNumbers = document.getElementById("include-numbers")
+const includeLowercase = document.getElementById("include-lowercase")
+const includeUppercase = document.getElementById("include-uppercase")
+const includeSymbols = document.getElementById("include-symbols")
 
 const btnGenerate = document.getElementById("generate-password")
 
@@ -27,6 +36,7 @@ toggle.addEventListener("change", (e) => {
         root.style.setProperty("--bg-color", "#1F2937")
         root.style.setProperty("--h1-color", "white")
         root.style.setProperty("--p-color", "#D5D4D8")
+        root.style.setProperty("--label-color", "white")
 
     } else {
         // Light Mode
@@ -34,11 +44,34 @@ toggle.addEventListener("change", (e) => {
         root.style.setProperty("--bg-color", "#ECFDF5")
         root.style.setProperty("--h1-color", "black")
         root.style.setProperty("--p-color", "#6B7280")
+        root.style.setProperty("--label-color", "black")
 
     }
 })
 
 btnGenerate.addEventListener("click", () => {
+    let characters = []
+
+    if(includeNumbers.checked) {
+        console.log("include numbers")
+        characters = characters.concat(numbers)
+    }
+
+    if(includeLowercase.checked) {
+        console.log("include lowercase")
+        characters = characters.concat(lowercase)
+    }
+
+    if(includeUppercase.checked) {
+        console.log("include uppercase")
+        characters = characters.concat(uppercase)
+    }
+
+    if(includeSymbols.checked) {
+        console.log("include symbols")
+        characters = characters.concat(symbols)
+    }
+
     let passwordArray = []
 
     while (passwordArray.length < 2) {
@@ -60,21 +93,23 @@ btnGenerate.addEventListener("click", () => {
 function copyToClipboard(e) {
     const text = e.textContent
 
-    navigator.clipboard.writeText(text)
-        .then(() => {
-            // console.log("Copied to clipboard:", text)
-            e.style.backgroundColor = "#10b981"
-            e.style.color = "#273549"
-            setTimeout(() => {
-                e.style.backgroundColor = "#273549"
-                e.style.color = "#55F991"
-            }, 100)
+    if (text != "") {
+        navigator.clipboard.writeText(text)
+            .then(() => {
+                // console.log("Copied to clipboard:", text)
+                e.style.backgroundColor = "#10b981"
+                e.style.color = "#273549"
+                setTimeout(() => {
+                    e.style.backgroundColor = "#273549"
+                    e.style.color = "#55F991"
+                }, 100)
 
-            setTimeout(() => {
-                alert(`Copied to clipboard`)
-            }, 150);
-        })
-        .catch(err => {
-            console.error("Failed to copy:", text)
-        })
+                setTimeout(() => {
+                    alert(`Copied to clipboard`)
+                }, 150);
+            })
+            .catch(err => {
+                console.error(`Failed to copy: ${err}`)
+            })
+    }
 }

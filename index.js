4f2caa4d819b2ea4b4e62661a3c7fdd19c1d2aaa
Anri20 <88647934+Anri20@users.noreply.github.com>
2025-03-26 07:21:40 +0700

    Styling
    Has Light Mode and Dark Mode

---
+++ index.js
@@ -0,0 +1,26 @@
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+    "/"];
+
+const main = document.querySelector("main")
+
+const toggle = document.getElementById("toggleSwitch")
+
+toggle.addEventListener("change", function (e) {
+    const root = document.documentElement
+
+    if (e.target.checked) {
+        // Dark Mode
+        console.log("checked")
+        root.style.setProperty("--bg-color", "#1F2937")
+        root.style.setProperty("--h1-color", "white")
+        root.style.setProperty("--p-color", "#D5D4D8")
+
+    } else {
+        // Light Mode
+        console.log("unchecked")
+        root.style.setProperty("--bg-color", "#ECFDF5")
+        root.style.setProperty("--h1-color", "black")
+        root.style.setProperty("--p-color", "#6B7280")
+
+    }
+})
